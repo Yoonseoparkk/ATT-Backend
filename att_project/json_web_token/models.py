a84be8ad0046efb7bb9531f7676674d790a89f2f
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-08-09 20:00:31 +0900

    fix: [허우영] models.py 주석 수정 [ATTB-11]

--- att_project/json_web_token/models.py
+++ att_project/json_web_token/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Custom User Manager Class로 사용자를 생성하고 관리자를 생성하는 메서드 입니다.
+# Custom User Manager Class로 사용자, 관리자를 생성하는 메서드
 class UserManager(BaseUserManager):
     # 주어진 이메일과 비밀번호를 일반 사용자 인스턴스로 생성합니다.
-    # 각각의 매게변수 설명
-    # email: 사용자의 이메일 주소
-    # password: 사용자의 비밀번호
-    # extra_field: 기본적인 이메일 주소, 비밀번호만 설정 했지만 나중에, MBTI, 나이, 생년월일을 설정할 수 있음
-    # return: 생성된 사용자 인스턴스
-    # raises ValueError: 이메일 (필수값)이 없으면 발생하는 에러 설정
+    # 각각의 매게변수 설명:
+    #   email: 사용자의 이메일 주소
+    #   password: 사용자의 비밀번호
+    #   extra_field: 기본적인 이메일 주소, 비밀번호만 설정 했지만 나중에, MBTI, 나이, 생년월일을 설정할 수 있음
+    # 반환값:
+    #   return: 생성된 일반 사용자 인스턴스
+    # 예외 처리:
+    #   raises ValueError: 이메일 (필수값)이 없으면 발생하는 에러 설정
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             # 이메일 필드는 필수로 제공되는 에러 문구 설정
@@ -19,19 +21,19 @@ class UserManager(BaseUserManager):
         email = self.normalize_email(email)
         # 사용자 모델의 인스턴스 생성
         user = self.model(email=email, **extra_fields)
-        # 비밀번호를 Hash화하여 설정
+        # 비밀번호를 Hashing하여 설정
         user.set_password(password)
-        # Database에 사용자 저장
+        # Database에 일반 사용자 저장
         user.save(using=self._db)
 
         return user
 
     # 주어진 이메일과 비밀번호로 관리자 인스턴스를 생성합니다.
-    # 각각의 매게변수 설명
-    # email, password, extra_fields, return 위와 동일
+    # 각각의 매게변수 설명:
+    #   email, password, extra_fields, return은 위쪽 코드의 create_user 메서드와 동일
     def create_superuser(self, email, password=None, **extra_fields):
         # 관리자 권한을 설정하기 위한 필드 기본값 설정
-        # 슈퍼유저 권한 부여
+        # 관리자 권한 부여
         extra_fields.setdefault('is_superuser', True)
         # 관리자 사이트 접근 권한 부여
         extra_fields.setdefault('is_staff', True)
