d158331710a2a34c87326854551af4fe6357e168
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-09-09 17:18:42 +0900

    feat: [허우영] 이메일과 비밀번호를 찾기 위한 엔드 포인트를 구현 [ATTB-25]

--- att_project/json_web_token/urls.py
+++ att_project/json_web_token/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from json_web_token.controller.user_controller import UserCreate, AuthAPIView, UserListView
+from json_web_token.controller.user_controller import UserCreate, AuthAPIView, UserListView, ForgotPasswordAPIView, \
+    ForgotEmailAPIView
+
 # json_web_token 앱 내에서 사용할 URL 패턴을 정의
 urlpatterns = [
     # 회원가입 URL
@@ -10,4 +12,8 @@ urlpatterns = [
 
     # 사용자 목록 조회
     path('users/', UserListView.as_view(), name='user-list'),
+
+    # 비밀번호 찾기와 이메일 찾기
+    path('forgot-password/', ForgotPasswordAPIView.as_view(), name='forgot_password'),
+    path('forgot-email/', ForgotEmailAPIView.as_view(), name='forgot_email'),
 ]
