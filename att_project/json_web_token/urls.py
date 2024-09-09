428ec99d23927c1c58aa362259b2cfc447caeb4e
heowooyoung <120430842+heowooyoung@users.noreply.github.com>
2024-09-09 17:16:05 +0900

    feat: [허우영] 관리자가 사용자 목록 조회를 위한 엔드 포인트 주소 추가 [

--- att_project/json_web_token/urls.py
+++ att_project/json_web_token/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from json_web_token.controller.user_controller import UserCreate, AuthAPIView
-
+from json_web_token.controller.user_controller import UserCreate, AuthAPIView, UserListView
 # json_web_token 앱 내에서 사용할 URL 패턴을 정의
 urlpatterns = [
     # 회원가입 URL
@@ -8,4 +7,7 @@ urlpatterns = [
 
     # 인증 관련 URL
     path('auth/', AuthAPIView.as_view(), name='auth'),
+
+    # 사용자 목록 조회
+    path('users/', UserListView.as_view(), name='user-list'),
 ]
